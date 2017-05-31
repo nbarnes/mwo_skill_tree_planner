bf4bb0c31294a4b1232f4818e6d089de52ce8b03
Van <van-mail@mail.ru>
2017-05-31 12:53:31 +0300

    Node class and some methods were moved to separate file
    Signed-off-by: Van <van-mail@mail.ru>

--- planner.js
+++ planner.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var allowFreeNodeSelection = true;
+var displayAllBonuses = false;
 document.addEventListener("DOMContentLoaded", function() {
 
   let maxSkillNodes = 91;
@@ -174,26 +176,6 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
   })();
-
-  function Node(newName, newAttribute, newValue, newValueTemplate, newId, newLeftChildId, newCenterChildId, newRightChildId) {
-    var selected = false;
-    let highlighted = false;
-
-    this.name = newName;
-    this.attribute = newAttribute;
-    this.value = newValue;
-    this.valueTemplate = newValueTemplate
-    this.id = newId;
-
-    this.leftChildId = newLeftChildId,
-    this.centerChildId = newCenterChildId,
-    this.rightChildId = newRightChildId;
-    this.parents = [];
-    this.children = [];
-    this.selected = selected;
-    this.highlighted = highlighted;
-    this.inDetachedSubTree = false;
-  }
 
   function buildUI(trees) {
     trees.forEach(function(tree, index) {
@@ -423,9 +405,6 @@ document.addEventListener("DOMContentLoaded", function() {
     return "child not found in getRelativeChildPosition()"
   }
 
-  var allowFreeNodeSelection = true;
-  var displayAllBonuses = false;
-  var highlightedNodesArray = [];
 
   function nodeClicked(node, e) {
     var multiSelectionHappened = false;
@@ -514,14 +493,6 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     }
   }
-  
-  function highlightNode(node) {
-    if(node) {
-      node.highlighted = true;
-      updateNodeColor(node);
-      highlightedNodesArray.push(node);
-    }
-  }
 
   function nodeUnHovered(node) {
     if(highlightedNodesArray.length == 0) {
@@ -532,81 +503,6 @@ document.addEventListener("DOMContentLoaded", function() {
       updateNodeColor(highlightedNode);
     }
     highlightedNodesArray = [];
-  }
-  
-  function selectNode(value, node) {
-//    console.log("nodeName " + node.name + " highlighted state is " + node.hightlighted);
-    node.selected = value;
-    if(value == false) {
-      node.inDetachedSubTree = false;
-      for (let childNode of node.children) {
-        if (childNode.selected && !childNode.inDetachedSubTree) {
-          childNode.inDetachedSubTree = true;
-          for (let parentNode of childNode.parents) {
-            if (parentNode.selected && !parentNode.inDetachedSubTree) {
-              childNode.inDetachedSubTree = false;
-              break;
-            }
-          }
-          if (childNode.inDetachedSubTree) {
-            propagateDetachment(childNode);
-          }
-        }
-      }
-    }
-    else {
-      if (!node.inDetachedSubTree) {
-        for(let childNode of node.children) {
-          if(childNode.inDetachedSubTree) {
-            childNode.inDetachedSubTree = false;
-            propagateAttachment(childNode);
-          }
-        }
-      }
-    }
-    updateNodeColor(node);
- 
-    for (let child of node.children) {
-      updateNodeColor(child);
-      //update color for other parents of children, so available ones for deselection before become locked if required.
-      for (let childParentNode of child.parents) {
-        if(childParentNode != node) {
-          updateNodeColor(childParentNode);
-        }
-      }
-    }
-    
-    for (let parent of node.parents) {
-      updateNodeColor(parent);
-    }
-  }
-  
-  function propagateAttachment(node) {
-    for (let childNode of node.children) {
-      if (childNode.inDetachedSubTree) {
-        childNode.inDetachedSubTree = false;
-        updateNodeColor(childNode);
-        propagateAttachment(childNode);
-      }
-    }
-  }
-  
-  function propagateDetachment(node) {
-    for (let childNode of node.children) {
-      if (childNode.selected && !childNode.inDetachedSubTree) {
-        childNode.inDetachedSubTree = true;
-        for (let parentNode of childNode.parents) {
-          if (parentNode.selected && !parentNode.inDetachedSubTree) {
-            childNode.inDetachedSubTree = false;
-            break;
-          }
-        }
-        if (childNode.inDetachedSubTree) {
-          updateNodeColor(childNode);
-          propagateDetachment(childNode);
-        }
-      }
-    }
   }
 
   function updateNodeColors(treeName) {
@@ -627,74 +523,7 @@ document.addEventListener("DOMContentLoaded", function() {
       selectNode(false, node);
     }
   }
-
-  function updateNodeColor(node) {
-    var mode = "";
-    if (node.highlighted)
-    {
-      mode = node.selected ? "highlightedSelected" : "highlightedNotSelected";
-    } else {
-      if (node.selected) {
-        if (safeToDeselect(node)) {
-          mode = "selected";
-        } else {
-          mode = "locked";
-        }
-      } else {
-        if ((allowFreeNodeSelection == false && nodeAvailableForSelection(node)) || node.parents.length == 0) {
-          mode = "available";
-        } else {
-          mode = "unavailable";
-        }
-      }
-    }
-    if(mode.length > 0) {
-//      console.log(node.name + " mode " + mode + " selected " + node.selected + " high " + node.highlighted);
-      setNodeElementColors(node, mode);
-    } else {
-      console.log("Can't determinate node color (mode) for " + node.name + " " + node.id);
-    }
-  }
-
-  function safeToDeselect(node) {
-    var safeToDeselect = true;
-    for (let child of node.children) {
-      if (child.selected) {
-        // Set node in question to deselected to see if the chlid is still elegible for selection
-        // based on other parents.  We"ll set it back to selected after we"re done with that check.
-        node.selected = false;
-        safeToDeselect = nodeAvailableForSelection(child) && safeToDeselect;
-        node.selected = true;
-      }
-    }
-    return safeToDeselect;
-  }
-
-  function nodeAvailableForSelection(node) {
-    var parentIsSelected = false;
-    for (let parentNode of node.parents) {
-      parentIsSelected = (parentNode.selected && !parentNode.inDetachedSubTree) || parentIsSelected;
-    }
-    parentIsSelected = parentIsSelected || (node.parents.length === 0);
-    return parentIsSelected;
-  }
-
-  function setNodeElementColors(node, state) {
-    document.getElementById(node.id).querySelectorAll(".node-element").forEach(function(element) {
-      removeNodeClasses(element);
-      element.classList.add(state);
-    });
-    let borderElement = "attached";
-    if(node.selected && node.inDetachedSubTree) {
-      borderElement = "detached";
-    }
-    document.getElementById(node.id).querySelectorAll(".hex-component").forEach(function(element) {
-      element.classList.remove("attached");
-      element.classList.remove("detached");
-      element.classList.add(borderElement);
-    });
-  }
-
+/*
   function updateNodeDisplay(treeName) {
     if (treeName == undefined) {
       for (let tree of SkillTree.getTrees()) {
@@ -706,7 +535,7 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     }
   }
-
+*/
   function updateNodeCounters(treeName) {
     let totalNodesSelected = SkillTree.getSelectedNodes().length;
     document.getElementById("node-selection-counter").textContent = totalNodesSelected;
@@ -796,15 +625,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
   function nodeNameToId(nodeName) {
     return nodeName.replace(/ /g, "-").toLowerCase();
-  }
-
-  function removeNodeClasses(nodeElement) {
-    nodeElement.classList.remove("selected");
-    nodeElement.classList.remove("available");
-    nodeElement.classList.remove("locked");
-    nodeElement.classList.remove("unavailable");
-    nodeElement.classList.remove("highlightedSelected");
-    nodeElement.classList.remove("highlightedNotSelected");
   }
 
   document.getElementById("reset-tree-button").addEventListener("click", function() {
@@ -1026,26 +846,6 @@ document.addEventListener("DOMContentLoaded", function() {
     updateNodeColors(treeName);
   }
   
-  function markAsAttachedRecursively(currentNodes)
-  {
-    let nextLevelNodes = [];
-    for (let currentNode of currentNodes) {
-      if (!currentNode.selected) {
-        continue;
-      }
-      for (let parentNode of currentNode.parents) {
-        if (parentNode.selected && !parentNode.inDetachedSubTree) {
-          currentNode.inDetachedSubTree = false;
-          nextLevelNodes = nextLevelNodes.concat(currentNode.children);
-          break;
-        }
-      }
-    }
-    if (nextLevelNodes.length > 0) {
-      markAsAttachedRecursively(nextLevelNodes);
-    }
-  }
-
   document.getElementById("settings-show-bonus-mode-button").addEventListener("click", function(event) {
     changeBonusMode();
     event.stopPropagation();
