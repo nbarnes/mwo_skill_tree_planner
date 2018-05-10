7385276ceb569bc2884ff887a66f8688fa8751b8
nbarnes <nbarnes@gmail.com>
2018-05-10 14:38:52 -0700

    Colorization pass on hexes and hex highlighting
    * Hover shadows on hexes are now tinted the same color as the hex
    * Borders on hexes are now a darker shade of the hex's color and lighten
    slightly when the attribute is moused over
    * Similar changes to uncolored mode hexes and hex borders

--- src/index.js
+++ src/index.js
@@ -19,8 +19,54 @@ document.addEventListener("DOMContentLoaded", function() {
   document.head.appendChild(colorizationStylesElement);
   let colorizationStyles = colorizationStylesElement.sheet;
   for (let attribute of Object.getOwnPropertyNames(attributeMap)) {
-    colorizationStyles.insertRule(`#graph-view.colorize-nodes .${Util.stringToCss(attribute)}.selected .hex-component { background-color: ${ attributeMap[attribute].color }`);
-    colorizationStyles.insertRule(`#graph-view.colorize-nodes .${Util.stringToCss(attribute)} .hex-component { background-color: ${ Util.shadeColor(attributeMap[attribute].color, -0.65) }`);
+
+    // background
+    // selected
+    colorizationStyles.insertRule(`.colorize-nodes .${Util.stringToCss(attribute)}.selected .hex-component { background-color: ${ attributeMap[attribute].color }`);
+    // unselected
+    colorizationStyles.insertRule(`.colorize-nodes .${Util.stringToCss(attribute)} .hex-component { background-color: ${ Util.shadeColor(attributeMap[attribute].color, -0.65) }`);
+
+    // box shadow
+    // selected, mouse over
+    colorizationStyles.insertRule(`.colorize-nodes ` +
+      `.${Util.stringToCss(attribute)}.mouse-over.selected ` +
+      `.hex-shadow-top { box-shadow: -1px -6px 15px 8px ${ Util.shadeColor(attributeMap[attribute].color, 0.5) }; }`);
+    colorizationStyles.insertRule(`.colorize-nodes ` +
+      `.${Util.stringToCss(attribute)}.mouse-over.selected ` +
+      `.hex-shadow-bottom { box-shadow: 0px 3px 15px 8px ${ Util.shadeColor(attributeMap[attribute].color, 0.5) }; }`);
+    // unselected, mouse over
+    colorizationStyles.insertRule(`.colorize-nodes ` +
+      `.${Util.stringToCss(attribute)}.mouse-over ` +
+      `.hex-shadow-top { box-shadow: -1px -6px 15px 8px ${ Util.shadeColor(attributeMap[attribute].color, 0.3) }; }`);
+    colorizationStyles.insertRule(`.colorize-nodes ` +
+      `.${Util.stringToCss(attribute)}.mouse-over ` +
+      `.hex-shadow-bottom { box-shadow: 0px 3px 15px 8px ${ Util.shadeColor(attributeMap[attribute].color, 0.3) }; }`);
+    // illegal, mouse over
+    colorizationStyles.insertRule(`#graph-view.colorize-nodes ` +
+      `.${Util.stringToCss(attribute)}.mouse-over.illegal ` +
+      `.hex-shadow-top { box-shadow: -1px -6px 15px 8px red; }`);
+    colorizationStyles.insertRule(`#graph-view.colorize-nodes ` +
+      `.${Util.stringToCss(attribute)}.mouse-over.illegal ` +
+      `.hex-shadow-bottom { box-shadow: 0px 3px 15px 8px red; }`);
+
+    // border
+    // selected, no mouse
+    colorizationStyles.insertRule(`.colorize-nodes ` +
+      `.${Util.stringToCss(attribute)}.selected .hex-component ` +
+      ` { border-color: ${ Util.shadeColor(attributeMap[attribute].color, -0.6) }`);
+    // selected, mouse over
+    colorizationStyles.insertRule(`.colorize-nodes ` +
+      `.${Util.stringToCss(attribute)}.selected.mouse-over .hex-component ` +
+      `{ border-color: ${ Util.shadeColor(attributeMap[attribute].color, -0.3) }`);
+    // unselected, no mouse
+    colorizationStyles.insertRule(`.colorize-nodes ` +
+      `.${Util.stringToCss(attribute)} .hex-component ` +
+      `{ border-color: ${ Util.shadeColor(attributeMap[attribute].color, -0.75) }`);
+    // unselected, mouse over
+    colorizationStyles.insertRule(`.colorize-nodes ` +
+      `.${Util.stringToCss(attribute)}.mouse-over .hex-component ` +
+      `{ border-color: ${ Util.shadeColor(attributeMap[attribute].color, -0.4) }`);
+
   }
 
   renderTree(skillTree);
