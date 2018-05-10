6020f5aa7186f1dfc5d16790b85b2c7d3da4d3ed
nbarnes <nbarnes@gmail.com>
2018-05-10 14:50:03 -0700

    Moved the attribute color insertion code out of index.js
    * CSS injection of the attribute<->color mapping rules moved out of
    index.js into attribute_colors.js

--- src/index.js
+++ src/index.js
@@ -10,65 +10,14 @@ import renderTree from "./render_tree.js";
 import wireEvents from "./event_wiring.js";
 import { loadFromRemoteId } from "./cold_storage.js";
 import { findById, findByClass } from "./dom.js";
+import insertAttributeColors from "./attribute_colors.js";
 
 document.addEventListener("DOMContentLoaded", function() {
 
   let skillTree = buildSkillTree(treeSource);
 
-  let colorizationStylesElement = document.createElement('style');
-  document.head.appendChild(colorizationStylesElement);
-  let colorizationStyles = colorizationStylesElement.sheet;
-  for (let attribute of Object.getOwnPropertyNames(attributeMap)) {
 
-    // background
-    // selected
-    colorizationStyles.insertRule(`.colorize-nodes .${Util.stringToCss(attribute)}.selected .hex-component { background-color: ${ attributeMap[attribute].color }`);
-    // unselected
-    colorizationStyles.insertRule(`.colorize-nodes .${Util.stringToCss(attribute)} .hex-component { background-color: ${ Util.shadeColor(attributeMap[attribute].color, -0.65) }`);
-
-    // box shadow
-    // selected, mouse over
-    colorizationStyles.insertRule(`.colorize-nodes ` +
-      `.${Util.stringToCss(attribute)}.mouse-over.selected ` +
-      `.hex-shadow-top { box-shadow: -1px -6px 15px 8px ${ Util.shadeColor(attributeMap[attribute].color, 0.5) }; }`);
-    colorizationStyles.insertRule(`.colorize-nodes ` +
-      `.${Util.stringToCss(attribute)}.mouse-over.selected ` +
-      `.hex-shadow-bottom { box-shadow: 0px 3px 15px 8px ${ Util.shadeColor(attributeMap[attribute].color, 0.5) }; }`);
-    // unselected, mouse over
-    colorizationStyles.insertRule(`.colorize-nodes ` +
-      `.${Util.stringToCss(attribute)}.mouse-over ` +
-      `.hex-shadow-top { box-shadow: -1px -6px 15px 8px ${ Util.shadeColor(attributeMap[attribute].color, 0.3) }; }`);
-    colorizationStyles.insertRule(`.colorize-nodes ` +
-      `.${Util.stringToCss(attribute)}.mouse-over ` +
-      `.hex-shadow-bottom { box-shadow: 0px 3px 15px 8px ${ Util.shadeColor(attributeMap[attribute].color, 0.3) }; }`);
-    // illegal, mouse over
-    colorizationStyles.insertRule(`#graph-view.colorize-nodes ` +
-      `.${Util.stringToCss(attribute)}.mouse-over.illegal ` +
-      `.hex-shadow-top { box-shadow: -1px -6px 15px 8px red; }`);
-    colorizationStyles.insertRule(`#graph-view.colorize-nodes ` +
-      `.${Util.stringToCss(attribute)}.mouse-over.illegal ` +
-      `.hex-shadow-bottom { box-shadow: 0px 3px 15px 8px red; }`);
-
-    // border
-    // selected, no mouse
-    colorizationStyles.insertRule(`.colorize-nodes ` +
-      `.${Util.stringToCss(attribute)}.selected .hex-component ` +
-      ` { border-color: ${ Util.shadeColor(attributeMap[attribute].color, -0.6) }`);
-    // selected, mouse over
-    colorizationStyles.insertRule(`.colorize-nodes ` +
-      `.${Util.stringToCss(attribute)}.selected.mouse-over .hex-component ` +
-      `{ border-color: ${ Util.shadeColor(attributeMap[attribute].color, -0.3) }`);
-    // unselected, no mouse
-    colorizationStyles.insertRule(`.colorize-nodes ` +
-      `.${Util.stringToCss(attribute)} .hex-component ` +
-      `{ border-color: ${ Util.shadeColor(attributeMap[attribute].color, -0.75) }`);
-    // unselected, mouse over
-    colorizationStyles.insertRule(`.colorize-nodes ` +
-      `.${Util.stringToCss(attribute)}.mouse-over .hex-component ` +
-      `{ border-color: ${ Util.shadeColor(attributeMap[attribute].color, -0.4) }`);
-
-  }
-
+  insertAttributeColors(attributeMap);
   renderTree(skillTree);
   wireEvents(skillTree);
   loadFromRemoteId(skillTree);
