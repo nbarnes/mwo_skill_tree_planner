3c030d82cc5eb1b9912658393e5ac9c33e052491
nbarnes <nbarnes@gmail.com>
2018-05-23 13:06:10 -0700

    * Colorization for SVG-based hex drawing
    * Node and node borders in attribute colors again
    * drop-shadow on hover for nodes works again
    * The dashed red border for illegally selected nodes is back, based on
    some Stack Overflow research and a big chunk of ugly code
    * viewbox tweaks on the hex template to account for the border, which
    was otherwise being cut off

--- src/attribute_colors.js
+++ src/attribute_colors.js
@@ -12,9 +12,27 @@ export default function insertAttributeColors(attributeMap) {
 
     // background
     // selected
-    colorizationStyles.insertRule(`.colorize-nodes .${Util.stringToCss(attribute.name)}.selected .hex-component { background-color: ${ Util.shadeColor(attribute.color, 0.2) }`);
+    colorizationStyles.insertRule(`.colorize-nodes .${Util.stringToCss(attribute.name)}.selected .hex-graphic { fill: ${ Util.shadeColor(attribute.color, 0.2) }`);
     // unselected
-    colorizationStyles.insertRule(`.colorize-nodes .${Util.stringToCss(attribute.name)} .hex-component { background-color: ${ Util.shadeColor(attribute.color, -0.55) }`);
+    colorizationStyles.insertRule(`.colorize-nodes .${Util.stringToCss(attribute.name)} .hex-graphic { fill: ${ Util.shadeColor(attribute.color, -0.55) }`);
+
+    // border
+    // selected, no mouse
+    colorizationStyles.insertRule(`.colorize-nodes ` +
+      `.${Util.stringToCss(attribute.name)}.selected .hex-graphic ` +
+      ` { stroke: ${ Util.shadeColor(attribute.color, -0.5) }`);
+    // selected, mouse over
+    colorizationStyles.insertRule(`.colorize-nodes ` +
+      `.${Util.stringToCss(attribute.name)}.selected.mouse-over .hex-graphic ` +
+      `{ stroke: ${ Util.shadeColor(attribute.color, -0.25) }`);
+    // unselected, no mouse
+    colorizationStyles.insertRule(`.colorize-nodes ` +
+      `.${Util.stringToCss(attribute.name)} .hex-graphic ` +
+      `{ stroke: ${ Util.shadeColor(attribute.color, -0.75) }`);
+    // unselected, mouse over
+    colorizationStyles.insertRule(`.colorize-nodes ` +
+      `.${Util.stringToCss(attribute.name)}.mouse-over .hex-graphic ` +
+      `{ stroke: ${ Util.shadeColor(attribute.color, -0.65) }`);
 
     // hex text
     colorizationStyles.insertRule(`.colorize-nodes ` +
@@ -28,42 +46,24 @@ export default function insertAttributeColors(attributeMap) {
     // selected, mouse over
     colorizationStyles.insertRule(`.colorize-nodes ` +
       `.${Util.stringToCss(attribute.name)}.mouse-over.selected ` +
-      `.hex-shadow-top { box-shadow: -1px -6px 15px 8px ${ Util.shadeColor(attribute.color, 0.5) }; }`);
-    colorizationStyles.insertRule(`.colorize-nodes ` +
-      `.${Util.stringToCss(attribute.name)}.mouse-over.selected ` +
-      `.hex-shadow-bottom { box-shadow: 0px 3px 15px 8px ${ Util.shadeColor(attribute.color, 0.5) }; }`);
+      `.hex-graphic { ` +
+      `-webkit-filter: drop-shadow(0px 0px 8px ${ Util.shadeColor(attribute.color, 0.5) }); ` +
+      `filter:         drop-shadow(0px 0px 8px ${ Util.shadeColor(attribute.color, 0.5) }); ` +
+      `}`);
     // unselected, mouse over
     colorizationStyles.insertRule(`.colorize-nodes ` +
       `.${Util.stringToCss(attribute.name)}.mouse-over ` +
-      `.hex-shadow-top { box-shadow: -1px -6px 15px 8px ${ Util.shadeColor(attribute.color, 0.3) }; }`);
-    colorizationStyles.insertRule(`.colorize-nodes ` +
-      `.${Util.stringToCss(attribute.name)}.mouse-over ` +
-      `.hex-shadow-bottom { box-shadow: 0px 3px 15px 8px ${ Util.shadeColor(attribute.color, 0.3) }; }`);
+      `.hex-graphic { ` +
+      `-webkit-filter: drop-shadow(0px 0px 8px ${ Util.shadeColor(attribute.color, 0.3) }); ` +
+      `filter:         drop-shadow(0px 0px 8px ${ Util.shadeColor(attribute.color, 0.3) }); ` +
+      `}`);
     // illegal, mouse over
     colorizationStyles.insertRule(`#graph-view.colorize-nodes ` +
       `.${Util.stringToCss(attribute.name)}.mouse-over.illegal ` +
-      `.hex-shadow-top { box-shadow: -1px -6px 15px 8px red; }`);
-    colorizationStyles.insertRule(`#graph-view.colorize-nodes ` +
-      `.${Util.stringToCss(attribute.name)}.mouse-over.illegal ` +
-      `.hex-shadow-bottom { box-shadow: 0px 3px 15px 8px red; }`);
-
-    // border
-    // selected, no mouse
-    colorizationStyles.insertRule(`.colorize-nodes ` +
-      `.${Util.stringToCss(attribute.name)}.selected .hex-component ` +
-      ` { border-color: ${ Util.shadeColor(attribute.color, -0.5) }`);
-    // selected, mouse over
-    colorizationStyles.insertRule(`.colorize-nodes ` +
-      `.${Util.stringToCss(attribute.name)}.selected.mouse-over .hex-component ` +
-      `{ border-color: ${ Util.shadeColor(attribute.color, -0.25) }`);
-    // unselected, no mouse
-    colorizationStyles.insertRule(`.colorize-nodes ` +
-      `.${Util.stringToCss(attribute.name)} .hex-component ` +
-      `{ border-color: ${ Util.shadeColor(attribute.color, -0.75) }`);
-    // unselected, mouse over
-    colorizationStyles.insertRule(`.colorize-nodes ` +
-      `.${Util.stringToCss(attribute.name)}.mouse-over .hex-component ` +
-      `{ border-color: ${ Util.shadeColor(attribute.color, -0.65) }`);
+      `.hex-graphic { ` +
+      `-webkit-filter: drop-shadow(0px 0px 8px red); ` +
+      `filter:         drop-shadow(0px 0px 8px red); ` +
+      `}`);
 
     // tooltip
     // tooltip border
