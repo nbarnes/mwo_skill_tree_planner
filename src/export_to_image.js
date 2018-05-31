b8f7914b5d7da6549ad3fa155efbebfcdd43ce9c
nbarnes <nbarnes@gmail.com>
2018-05-31 14:29:29 -0700

    WIP using dom-to-image
    * Not currently functional
    * dom-to-image is being stubborn, so I'm saving this for later and
    trying html2canvas next
    * Rando font stuff because I fiddle with stuff when I get bored

--- src/export_to_image.js
+++ src/export_to_image.js
@@ -1,21 +1,44 @@
+
 "use strict";
 
 import domtoimage from 'dom-to-image';
+import { treeSource } from "./tree_source";
 import { findById } from "./dom.js";
+import { treeNameToId } from "./util.js";
 
 export let saveToFile = function (rootNode) {
-  // domtoimage.toPng(findById('graph-view'))
-  domtoimage.toJpeg(findById('graph-view'), { quality: 0.7 })
+
+  domtoimage.toPng(findById('graph-view'))
+  // domtoimage.toJpeg(findById('graph-view'), { quality: 0.7 })
   .then( dataUrl => {
 
-    // var img = new Image();
-    // img.src = dataUrl;
-    // findById('graph-view').appendChild(img);
+    var img = new Image();
+    img.src = dataUrl;
+    findById('graph-view').appendChild(img);
 
-    var link = document.createElement('a');
-    link.download = 'skilltree.jpeg';
-    link.href = dataUrl;
-    link.click();
+  // var link = document.createElement('a');
+  // link.download = 'skill_tree.jpeg';
+  // link.href = dataUrl;
+  // link.click();
 
   });
+
+  // findById('graph-view').appendChild( buildTreeView() );
+
+}
+
+function buildTreeView() {
+
+  let treeView = findById("tree-view-template").content.cloneNode(true).firstElementChild;
+  treeView.id = 'tree-view';
+
+  for (let tree of treeSource) {
+    let treeElement = findById(treeNameToId(tree.treeName)).cloneNode(true);
+    treeElement.id = '';
+    treeElement.classList.remove('hide');
+    treeView.appendChild(treeElement);
+  }
+
+  return treeView;
+
 }
